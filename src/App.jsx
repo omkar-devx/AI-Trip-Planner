64e25dd58f6ac12d9c3dc2c15191cbe448b40844
Omkar Prajapati <omkar.devx@gmail.com>
2024-11-15 12:33:10 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Hero from "./components/custom/Hero";
+
+function App() {
+  return (
+    <div className="">
+      <Hero />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/custom/Hero.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Button } from "../ui/button";
+import { Link } from "react-router-dom";
+
+const Hero = () => {
+  return (
+    <div className="flex flex-col items-center mx-56 my-16 gap-9 ">
+      <h1 className="font-extrabold text-[45px] text-center">
+        <span className="text-[#f56551]">
+          Discover Your Next Adventure with AI:
+        </span>
+        <br />
+        Personal Itineraries at Your Fingertips
+      </h1>
+      <p className="text-xl text-gray-500 text-center">
+        Your Personal trip planner and travel curator, creating custom
+        itineraries tailored to your interest and budget
+      </p>
+      <Link to={"/create-trip"}>
+        <Button> Get Started, It's Free</Button>
+      </Link>
+    </div>
+  );
+};
+
+export default Hero;
