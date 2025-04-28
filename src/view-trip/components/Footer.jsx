6be91b9e3f9ce68eb0822624c696a07690d0c87e
Omkar Prajapati <omkar.devx@gmail.com>
2025-04-28 10:45:31 +0530

    Update Footer.jsx

--- src/view-trip/components/Footer.jsx
+++ src/view-trip/components/Footer.jsx
@@ -4,10 +4,10 @@ const Footer = ({ trip }) => {
   return (
     <div>
       <h2 className="font-bold mt-10 text-center text-orange-600">
-        ~
-        <a href="https://github.com/omkar-devx" target="_blank">
-          omakrdevx
-        </a>
+        //~
+        //<a href="https://github.com/omkar-devx" target="_blank">
+        //  omakrdevx
+        //</a>
       </h2>
     </div>
   );
